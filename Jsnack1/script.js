798ecfdd62ed91444dce139dea692d2e11a3ced4
Alberto-Bruno <brunoalberto97@outlook.it>
2022-12-14 13:26:50 +0100

    Snack1

--- Jsnack1/script.js
+++ Jsnack1/script.js
@@ -8,9 +8,23 @@ const userElement = document.getElementById('user');
 
 // chiedo per 10 volte all'utente di inserire un numero
 
-for (let i = 0; i <= 10; i++) {
-    
+let sum = 0;
+
+let isValid = true;
+
+for (let i = 0; i < 10; i++) {
+    const number = parseInt(prompt('inserisci il numero', 1).trim());
+    if (isNaN(number)) {
+        isValid =false;
+    } else {
+        sum += number;
+    }
 }
+
+const message = isValid ? `la somma dei tuoi numer è ${sum}` : 'Hai inserito valori non validi';
+
+userElement.innerText = message;
+
 
 
 
